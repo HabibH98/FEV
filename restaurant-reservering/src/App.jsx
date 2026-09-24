bc4507267f33087654dd395f901d829c75298a1d
HabibH98 <97081247@st.deltion.nl>
2026-09-24 13:46:38 +0200

    update

---
+++ restaurant-reservering/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Mijn Restaurant</h1>
+      <p>Welkom bij onze restaurantwebsite.</p>
+      <button>Reserveer een tafel</button>
+    </div>
+  )
+}
+
+export default App
---
+++ restaurant-reservering/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Mijn Restaurant</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Over ons</li>
+        <li>Reserveren</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
